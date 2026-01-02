94fb4534d48b44d299ad4c888b260bb52f8b02b4
Otroveno <venomdu73@gmail.com>
2026-01-02 07:10:44 +0100

    Delete Skald directory

--- Skald/src/CampaignSystem/Current.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Skald.src.CampaignSystem
-{
-    public static class Current
-    {
-        // Vos propriétés et méthodes statiques
-        public static object SomeProperty { get; set; }
-    }
-}
