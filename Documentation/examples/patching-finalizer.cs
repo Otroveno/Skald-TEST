6f510362b8f3caa05a9c1d30160ead29e16ed570
Otroveno <venomdu73@gmail.com>
2026-01-02 23:34:21 +0100

    Add files via upload

---
+++ Documentation/examples/patching-finalizer.cs
@@ -0,0 +1,24 @@
+namespace Patching_Finalizer
+{
+	using System;
+	using System.Runtime.Serialization;
+
+	class Foo { }
+	class Bar { }
+
+	class Example
+	{
+		// <rethrow>
+		[Serializable]
+		public class MyException : Exception
+		{
+			public MyException() { }
+			public MyException(string message) : base(message) { }
+			public MyException(string message, Exception innerException) : base(message, innerException) { }
+			protected MyException(SerializationInfo serializationInfo, StreamingContext streamingContext) => throw new NotImplementedException();
+		}
+
+		static Exception Finalizer(Exception __exception) => new MyException("Oops", __exception);
+		// </rethrow>
+	}
+}
